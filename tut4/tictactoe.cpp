080c3ee20e0113e07e5e63e845aa89cb75c8957d
neil13245 <warren13245@gmail.com>
2016-03-16 11:05:24 +0200

    tut4 commit

---
+++ tut4/tictactoe.cpp
@@ -0,0 +1,125 @@
+#include "tictactoe.h"
+
+using namespace std;
+
+tictactoe::tictactoe()
+{
+	reset();
+}
+
+//Clear Table
+void tictactoe::reset()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int t = 0; t < 3; t++)
+		{
+			grid[i][t] = ' ';
+		}
+
+	}
+}
+
+bool tictactoe::move(int x, int y)
+{
+	if (grid[x][y] != ' ')
+	{
+		if (int i = 1)
+		{
+			grid[x][y] = 'x';
+		}
+		else
+		{
+			grid[x][y] = 'o';
+		}
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void tictactoe::print()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int t = 0; t < 3; t++)
+		{
+			cout << grid[i][t] << " " << endl;
+		}
+		cout << endl;
+	}
+}
+
+bool tictactoe::over()
+{
+	char checkx = wincheck('x');
+	char check0 = wincheck('o');
+	if (check0 == 'o'&& check0 == 'x')
+	{
+		player = 'd';
+		return false;
+	}
+	if (checkx == 'x')
+	{
+		player = 'x';
+		return true;
+	}
+	if (checkx == 'o')
+	{
+		player = 'o';
+		return true;
+	}
+	return false;
+}
+
+bool tictactoe::over()
+{
+	char checkx = wincheck('x');
+	char check0 = wincheck('o');
+	if (check0 == 'o'&& check0 == 'x')
+	{
+		player = 'd';
+		return false;
+	}
+	if (checkx == 'x')
+	{
+		player = 'x';
+		return true;
+	}
+	if (checkx == 'o')
+	{
+		player = 'o';
+		return true;
+	}
+	return false;
+}
+
+char tictactoe::wincheck(char p)
+{
+
+	for (int z = 0; z < 3; z++)
+	{
+		if (grid[z][0] &= grid[z][1] &= grid[z][2])
+		{
+			return p;
+		}
+	}
+
+	for (int z = 0; z < 3; z++)
+	{
+		if (grid[0][z] &= grid[1][z] &= grid[1][z])
+		{
+			return p;
+		}
+	}
+
+	if (grid[0][0] &= grid[1][1] &= grid[2][2])
+	{
+		return p;
+	}
+
+	return ' ';
+
+}
---
+++ tut4/tictactoe.h
@@ -0,0 +1,45 @@
+#pragma once
+#pragma once\
+
+
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class tictactoe
+{
+private:
+	int move;
+	char player;
+	char grid[3][3];
+
+public:
+	tictactoe();
+	void reset();
+	bool move(int, int);
+	void print();
+	bool over();
+	char wincheck(char);
+	~tictactoe();
+
+
+};
+int main()
+{
+	tictactoe myGame;
+	int x;
+	int y;
+	cout << "Welcome to Tic Tac Toe" << endl;
+	while (true)
+	{
+		myGame.print();
+		cout << "Enter Collum";
+		cin >> x;
+		cout << "Enter Row";
+		cin >> y;
+		bool play = myGame.move(x, y);
+
+	}
+}
